787988c0b2b516adf6f47e0e28d89165f2069bf6
user <1001inchnails@gmail.com>
2025-01-20 17:04:21 +0100

    last00

--- api/app.js
+++ api/app.js
@@ -1,3 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 async function conectarCliente(){    // funcion para conexion a cliente...
   const { MongoClient, ServerApiVersion } = require('mongodb');
   const uri = "mongodb+srv://dccAtlMongoC_S:1001%25%25wWqq4904@clusterbuster.bl5p1.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBuster";
@@ -34,13 +40,6 @@ async function insertarNuevoDocumento(nuevoDoc) { // inserta nuevo documento en 
     await cliente.close();
   }
 }
-
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.json({
@@ -117,10 +116,4 @@ app.post('/api/datos', async(req,res)=>{
 });
 
 module.exports = app;
-
-/* const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
- */
 
